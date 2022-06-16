ae641ae7dc4f2869030d532ce04bbc738c6e9c95
Bruno Chirelli <brunochirelli@gmail.com>
2022-06-16 10:55:12 -0300

    add: utils package

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,24 @@
 import Link from "next/link";
+import { useState } from "react";
 import { Button } from "ui";
+import { sum } from "utils";
 
 export default function Web() {
+  const [a, setA] = useState("");
+  const [b, setB] = useState("");
+
   return (
     <div>
       <h1>Web</h1>
+
+      <input type="number" onChange={(e) => setA(e.target.value)} />
+      <input type="number" onChange={(e) => setB(e.target.value)} />
+
+      <p>Hey, tricky question</p>
+      <p>
+        How much is {a} + {b}?
+      </p>
+      <p>Maybe {sum(Number(a), Number(b))}</p>
 
       <Link href={"/login"}>Login</Link>
     </div>
