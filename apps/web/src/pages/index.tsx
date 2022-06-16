12b46f48d6a12317c9a5a763bce6ff95d584ecd5
Bruno Chirelli <brunochirelli@gmail.com>
2022-06-16 16:12:18 -0300

    update: redux hook location

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { Button } from "ui";
 import { sum } from "utils";
-import { useAppDispatch, useAppSelector } from "../features/hooks/redux";
+import { useAppDispatch, useAppSelector } from "../features/common/hooks/redux";
 import { setUser } from "../features/user/store/userSlice";
 
 export default function Web() {
